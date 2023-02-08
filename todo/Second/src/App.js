175b8773deb00a1d3506e0268336c18043fba764
sangmin131 <smha77@naver.com>
2023-02-08 18:20:49 +0900

    123

---
+++ todo/Second/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import TodoListTemplate from './com/TodoListTemplate';
+
+function App() {
+
+
+  return (
+    <>
+      <TodoListTemplate/>
+    </>
+
+
+  );
+}
+
+export default App;
